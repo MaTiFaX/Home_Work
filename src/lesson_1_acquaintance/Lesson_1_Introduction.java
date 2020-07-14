b38834a22da8ee5a907845517d5845dd5c9185be
MaTiFaX <matifax123@mail.ru>
2020-07-14 15:41:30 +0500

    Initial commit

---
+++ src/lesson_1_acquaintance/Lesson_1_Introduction.java
@@ -0,0 +1,14 @@
+package lesson_1_acquaintance;
+
+    public class Lesson_1_Introduction {
+        public static void main(String[] args) {
+            for(int i = 0; i < 5; i++) {
+                System.out.print("Привет! меня зовут Кирилл. ");
+            }
+            System.out.println();
+
+            for(int i = 0; i < 5; i++) {
+                System.out.println("Я хочу быть Java-разработчиком!");
+            }
+        }
+    }
